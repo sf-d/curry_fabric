f3172fd11e1da16895979342605c5433d12716cf
Your Name <you@example.com>
2022-03-02 11:49:27 +0300

    f

--- src/curry_fabric/curriedfunc.py
+++ src/curry_fabric/curriedfunc.py
@@ -18,12 +18,12 @@ from itertools import product
 #list_test = (list(product([0.1, 0.4], repeat = len(args))))
 
 def curried(func):
-    def curry(*args, **kwargs):
-        if len(args)+len(kwargs) == func.__code__.co_argcount:
-            ans = func(*args, **kwargs)
+    def curry(*args):
+        if len(args) == func.__code__.co_argcount:
+            ans = func(*args)
             return ans
         else:
-            return (lambda *x, **y: curried(*(args + x), **dict(kwargs ,y)))     
+            return (lambda *x: curried(*(args + x)))     
             
     return curry
 
@@ -31,14 +31,12 @@ def curried(func):
 
 @curried
 def test(lk, k, p, pd, ld):
-    list_test = (list(product([0.1, 0.5, 1] , repeat = 5)))
-    for i in list_test:
-        return lk*((k*p*pd)/ld)  
+    return lk*((k*p*pd)/ld)  
 
 
 list_test = (list(product([0.1, 0.5, 1] , repeat = 5)))
 for i in list_test:
-    p = test(i[0], i[1], i[2], i[3], i[4])
+    p = test(*(i))
     print (i,':', p)
       
         
