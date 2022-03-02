55d810e212fb92eb850c72a6f8046607193d73a9
Your Name <you@example.com>
2022-03-03 01:40:34 +0300

    l

--- src/curry_fabric/curriedfunc.py
+++ src/curry_fabric/curriedfunc.py
@@ -2,33 +2,34 @@ import math
 import operator
 import functools
 from itertools import product
+import numpy as np
 
 
 def curried(func):
     def curry(*args):
         if len(args) == func.__code__.co_argcount:
-            ans = func(*args)
-            return ans
+            return func(*args)
         else:
-            return (lambda *x: curried(*(args + x)))     
-            
+            return (lambda *x: curried(*(args + x)))              
     return curry
 
 
-function = {('lk','k', 'p', 'pd', 'ld'): 'lk*((k*p*pd)/ld'}
 
 
 @curried
-def Sblock (s_d):
-    return (2*1000 / s_d)**2
-
 def test(lk, k, p, pd, ld):
-    return lk*((k*p*pd)/ld)  
+    return lk*((k*p*pd)/ld) 
 
 
-list_test = (list(product([0.1, 0.5, 1] , repeat = test.__code__.co_argcount)))
+
+
+
+lk = [10,20,30]
+
+pp = [3,4,5]
+
+list_test = (list(product(pp , repeat = 5)))
 for i in list_test:
-    
     p = test(*(i))
     print (i,':', p)
     
