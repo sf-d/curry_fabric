db9fadc5e75647acf3167e9cb033a893358948d3
Your Name <you@example.com>
2022-03-06 04:17:39 +0300

    rr

--- src/curry_fabric/curriedfunc.py
+++ src/curry_fabric/curriedfunc.py
@@ -1,61 +1,94 @@
-import math
-import operator
-import functools
-from itertools import product, repeat
-from textwrap import wrap
+from functools import reduce
+from itertools import product
+
 import numpy as np
-np.set_printoptions(suppress=True)
-STEPS = 3
+import sys
+import plotly.express as px
+
+
+
+
 class p:
-    def __init__ (self, n, intrv):
+    def __init__(self, n, intrv):
         self.n = n
         self.start, self.stop = intrv
         self.intrv = intrv
         self.vals = np.linspace(self.start, self.stop, n)
 
 
+def curry(func: callable) -> callable:
+    """
+    :rtype: callable
+    """
 
-def curry(func):
     def curried(*args):
+
         if len(args) == func.__code__.co_argcount:
-            steps_=set()
-            to_pr=[]
-            for arg in args:
-                v = arg.vals
-                to_pr.append(v)
-                steps_.add(len(v))
-            if len(steps_)==1:
+            steps_: set[int] = set()
+
+            steps_.update([len(arg.vals) for arg in args])  # сет получаем через update чтобы облегчить синтаксис
+
+            to_pr = [arg.vals for arg in args]  # упрощаем цикл
+            if len(steps_) == 1:
+                # векторизируем функцию под numpy аргументы, чтобы избежать длинного цикла подстановки
+                vfunc = np.vectorize(func)
+                
                 list_test = np.asarray(list(product(*to_pr)))
-                answs=[]
-                for i in list_test:
-                    p = func(*(i))
-                    answs.append(p)
-                reshp_list=list_test.reshape((*repeat(list(steps_)[0], func.__code__.co_argcount),func.__code__.co_argcount))
-                reshp_answs=np.asarray(answs).reshape((*repeat(list(steps_)[0],func.__code__.co_argcount),1))
-                return reshp_list, reshp_answs
+                answers = vfunc(*list_test.T)
+                
+                return np.c_[list_test, answers]
             else:
-                print(f'steps count not identic: {steps_}')
+                print(f'steps count not identical: {steps_}')
         else:
-            return (lambda *x: curried(*(args + x)))              
+            return lambda *x: curried(*(args + x))
+
     return curried
 
 
+np.set_printoptions(precision=3, threshold=sys.maxsize, suppress=True)
 
-liv_lot = p(STEPS,(100000,500000))
-kit = p(STEPS,(1,10))
-liv_dens = p(STEPS,(1,10))
-child = p(STEPS,(10,100))
-ch_lot_dens = p(STEPS,(1,10))
+STEPS = 15
+
+L = p(STEPS, (5000000, 50000000))
+A = p(STEPS, (1000000, 5000000))
 
 
 @curry
-def S_child_lot(liv_lot, kit, liv_dens, child, ch_lot_dens): 
-    return liv_lot*((kit)/(liv_dens/10000)*child*(ch_lot_dens/10000)) 
+def D_sn(L, A):
+    return L / A
 
-list_t, ans = S_child_lot(liv_lot, kit, liv_dens, child, ch_lot_dens)
+test_ar = D_sn(L, A)
 
 
-print (list_t[0,0,0,:,0])
+#Целевые показатели
+D_sn_goal = 15
+A_goal = 2600000
+L_goal = 39000000
+goal_ar = np.array([L_goal, A_goal, D_sn_goal])
 
-print (list_t[1,1,1,:,1])
 
+for t in test_ar:
+    dist = np.linalg.norm(t- goal_ar)
+    print(dist)
+    
+K = 5
+nearest_partition = np.argpartition(dist, K + 1, axis=1)
+
+
+# operations with array if we have more then 3 values
+"b = np.sum(a, where=[True,True,True,True, False,False], axis = 1, keepdims = True)"
+'#c = np.take(a,[4,5], axis=1)'
+'#result = np.append(b, c, axis=1)'
+
+
+list_a=np.take(test_ar, [0], axis =1)
+list_aa = list_a.flatten()
+
+list_b = np.take(test_ar, [1], axis =1)
+list_bb = list_b.flatten()
+
+list_c = np.take(test_ar, [2], axis =1)
+list_cc = list_c.flatten()
+
+fig = px.scatter_3d(x=list_aa, y=list_bb, z=list_cc, text=range(len(list_bb)))
+fig.show()
